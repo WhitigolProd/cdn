5962e0542aeaa6a4d4346db9dca3490f09c338a7
WhitigolProd <36452182+WhitigolProd@users.noreply.github.com>
2022-05-01 18:56:24 -0400

    Create CDN File

---
+++ cdn.js
@@ -0,0 +1,5 @@
+let curyear = new Date().getFullYear();
+const cosmicCDN = {
+    info: 'Cosmic Systems Content Delivery Network',
+    rights: `Copyright (c) ${curyear} Cosmic Systems - All Rights Reserved.`,
+}
