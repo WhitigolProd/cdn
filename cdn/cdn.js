d2afd4701bedd622baa4c809886e785177cf202e
WhitigolProd <36452182+WhitigolProd@users.noreply.github.com>
2022-05-01 22:30:48 -0400

    Update cdn.js

--- cdn/cdn.js
+++ cdn/cdn.js
@@ -3,3 +3,5 @@ const cosmicCDN = {
     info: 'Cosmic Systems Content Delivery Network',
     rights: `Copyright (c) ${curyear} Cosmic Systems - All Rights Reserved.`,
 }
+
+// Served by Cosmic Systems CDN on Github
